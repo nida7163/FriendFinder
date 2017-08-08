291bd463690f2882becd0645b765e2f5dec1c56d
Chaudhry <nida.chaudhry@hawks.com>
2017-08-08 09:41:56 -0400

    initial commit

---
+++ app/data/friend.js
@@ -0,0 +1,53 @@
+var friends = [
+	
+	{
+		name: "Beyoncé Knowles",
+		photo: "http://www.billboard.com/files/media/beyonce-formation-live-billboard-2016-1548.jpg",
+		scores: [
+			"5",
+			"1",
+			"4",
+			"3",
+			"1",
+			"5",
+			"1",
+			"4",
+			"3",
+			"1"
+		]
+	},
+	{
+		name: "Hillary Clinton",
+		photo: "http://jtf.org/wp-content/uploads/2015/08/hillary-clinton-laughing.jpg",
+		scores: [
+			"3",
+			"1",
+			"5",
+			"4",
+			"1",
+			"5",
+			"1",
+			"1",
+			"5",
+			"3"
+		]
+	},
+	{
+		name: "Oprah Winfrey",
+		photo: "http://i.huffpost.com/gen/1588485/images/o-OPRAH-facebook.jpg",
+		score: [
+			"3",
+			"1",
+			"4",
+			"1",
+			"4",
+			"3",
+			"5",
+			"1",
+			"5",
+			"4"		
+		]
+	}
+];
+
+module.exports = friends;
---
+++ server.js
@@ -0,0 +1,21 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+
+// Tells node that we are creating an "express" server
+var app = express();
+
+// Sets an initial port. We"ll use this later in our listener
+var PORT = process.env.PORT || 8080;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+
+require("./app/routing/htmlRoutes")(app);
+
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
